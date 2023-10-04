64aeea679fb93b3759d433e353ec9ddfa7c4c74d
Protullen <138602811+protullen@users.noreply.github.com>
2023-10-04 18:25:09 +0530

    Update main.py

--- main.py
+++ main.py
@@ -2,22 +2,22 @@ from pyrogram import Client, filters, enums
 
 app = Client(
   'autocaption',
-  api_id = 6353248,
-  api_hash = '1346f958b9d917f0961f3e935329eeee',
-  bot_token = '6285956621:AAF16zJce7vXr3wukHJDO9qOYpXQ-AcSInU'
-)  
+  api_id=6353248,
+  api_hash='1346f958b9d917f0961f3e935329eeee',
+  bot_token='6285956621:AAF16zJce7vXr3wukHJDO9qOYpXQ-AcSInU'
+)
 
 @app.on_message(filters.command('start'))
 async def start(bot, update):
-    await update.reply("Hello I'm Auto Caption Bot")
-    
+    await update.reply("Hello, I'm Auto Caption Bot")
+
 @app.on_message((filters.video | filters.document) & filters.channel)
 async def autocaption(bot, update):
     await bot.edit_media_caption(
-        caption = 'This Caption From Bot',
-        message_id = update.id,
-        chat_id = update.chat.id,
-        patse_mode = enums.ParseMode.MARKDOWN
+        caption='This Caption From Bot',
+        message_id=update.id,
+        chat_id=update.chat.id,
+        parse_mode=enums.ParseMode.MARKDOWN
     )
 
 app.run()
