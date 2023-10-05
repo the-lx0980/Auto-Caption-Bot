856fa28fda0a15ac28ef1a13d43caa537a8db5e6
Protullen <138602811+protullen@users.noreply.github.com>
2023-10-05 23:12:24 +0530

    Update main.py

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ app = Client(
   'autocaption',
   api_id=6353248,
   api_hash='1346f958b9d917f0961f3e935329eeee',
-  bot_token='6285956621:AAF16zJce7vXr3wukHJDO9qOYpXQ-AcSInU'
+  session_string="BQGb9AQAC5V8laNyrwshIL9_Z-Lx9aYFZBwdr58BH3Fgo6eGXjUonmIyfFMAUeNWHuVcDjuMWVMRvGhQCQh3Ab2BMPUYnAOu_ZAvdyg4SJXC1r2IV5Ot7XtEFmImYGKvKGUkPR0_kHGxEcwftFYy7y7fxtG1a8R9_VCTrzJVHePIfRyTkEy3jkUKWH2Ce12ZaDrulqTNaEZR8NtZ8pTGNyg-9kKH5ahTwS8N5MBCje7RMzLvj0-zCpJAgn3tZPbGk3MLNJR1gY7qQKiR7Qm7KnvyuoKG_nTDMd7z3Yvg2Fj01XWu9Kqm5r4y8v9wWR12CNBtl1B-2FLdogpQtTgyPXGrQwMbZAAAAAFM4O0NAA"
 )
 
 @app.on_message(filters.command('start'))
