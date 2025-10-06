5d7ba24806024217228f9500a8dbd6991e67ef7a
lx0980 <138602811+lx0980@users.noreply.github.com>
2025-10-06 21:02:12 +0530

    Update projects.py

--- projects.py
+++ projects.py
@@ -1,12 +1,12 @@
 PROJECTS = {
     "File Streamer": {
-        "app_url": "https://file-streamer1.onrender.com",
-        "deploy_url": "https://api.render.com/deploy/srv-111?key=KEY1"
+        "deploy_url": "https://api.render.com/deploy/srv-cj8tea8eba7s73fvadu0?key=aZgM2q3f5pY",
+        "app_url": "https://webxzonebot.onrender.com"
     },
-    "Video Stream": {
-        "app_url": "https://video-stream1.onrender.com",
-        "deploy_url": "https://api.render.com/deploy/srv-112?key=KEY2"
-    },
+    "Channel Clone": {
+        "deploy_url": "https://api.render.com/deploy/srv-cs7j8lbv2p9s73f3dksg?key=Midzpimsd88",
+        "app_url": "https://the-cloner-boy-cx2q.onrender.com"
+    }, 
     "Uptime Monitor": {
         "app_url": "https://uptime-monitor1.onrender.com",
         "deploy_url": "https://api.render.com/deploy/srv-113?key=KEY3"
